3601024accc6ff8672c4fe289aab1bfc90ef1ee6
Bren <brendon.bolles@gmail.com>
2017-04-15 17:32:46 -0400

    Debug statements in api routs to determine why user is always denied access on azure server but not local.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,38 +1,42 @@
 var restify = require('restify');
 
+var day = new Date().getDay();
+
+var time = new Date().getHours();
+
 
 module.exports =  function(router, passport){
-	
-	router.get('/', function(req, res){
-		res.send('api with restful send!');
-	});
 
-	
-	router.post('/',  passport.authenticate('apiCheck',  {
-		session: false,
-		failureRedirect: '/api/denied'
-		
-	 	
-	 }),
-	function(req, res){
-		res.json('Authenticated!');
-	});
+    router.get('/', function(req, res){
+        res.send('api with restful send!');
+    });
 
 
+    router.post('/',  passport.authenticate('apiCheck',  {
+            session: false,
+            failureRedirect: '/api/denied'
 
 
-	router.get('/authenticated', passport.authenticate('basic',  {session: false}), function(req, res){
-		// if(!req.isAuthenticated())
-		// 	res.redirect('/api/potato')
-		res.json('Authenticated!')
-	});	
+        }),
+        function(req, res){
+            res.json('Authenticated!');
+        });
 
 
-	router.get('/denied', function(req, res){
-		res.json('Denied!');
-	});	
 
 
-	
+    router.get('/authenticated', passport.authenticate('basic',  {session: false}), function(req, res){
+        // if(!req.isAuthenticated())
+        // 	res.redirect('/api/potato')
+        res.json('Authenticated!')
+    });
 
-}
+
+    router.get('/denied', function(req, res){
+        res.json('Denied! The day is ' + day + ' The hour is ' + time);
+    });
+
+
+
+
+};
