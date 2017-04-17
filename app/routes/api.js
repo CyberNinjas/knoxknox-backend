95d41e555459a273a2a7847a9381a7372245fe07
Bren <brendon.bolles@gmail.com>
2017-04-17 11:58:03 -0400

    Debug code to find out format data server is receiving from arduino.

--- app/routes/api.js
+++ app/routes/api.js
@@ -17,7 +17,7 @@ module.exports =  function(router, passport){
         res.send('api with restful send!');
     });
 
-    router.post('/', jsonParser, function(req, res){
+    router.post('/', function(req, res){
         res.send(req.body);
     });
 
