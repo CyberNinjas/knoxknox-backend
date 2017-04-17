512ca1c346035c1091cda2d27c44aa97f456a07a
Bren <brendon.bolles@gmail.com>
2017-04-17 11:44:42 -0400

    Debug code to find out format data server is receiving from arduino.

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,26 +5,37 @@ var day = new Date().getDay();
 var time = new Date().getHours();
 
 
+var bodyParser = require('body-parser');
+
+
+var jsonParser = bodyParser.json();
+
+
 module.exports =  function(router, passport){
 
     router.get('/', function(req, res){
         res.send('api with restful send!');
     });
 
-
-    // router.post('/',  passport.authenticate('apiCheck',  {
-    //         session: false,
-    //         failureRedirect: '/api/denied'
-    //
-    //
-    //     }),
-    //     function(req, res){
-    //         res.json('Authenticated!');
-    //     });
-
-    router.post('/', function(req, res){
-       res.send(req.body);
+    router.post('/', jsonParser, function(req, res){
+        res.send(req.body);
     });
+
+    // router.post('/', function(req, res, next) {
+    //  		passport.authenticate('apiCheck', function(req, err, user, info) {
+    //  			console.print(req.body);
+    // 	    if (err) {
+    // 	      return next(err); // will generate a 500 error
+    // 	    }
+    // 	    // Generate a JSON response reflecting authentication status
+    // 	    if (! user) {
+    // 	      return res.send('Denied! Data from arduino: ' + data);
+    // 	    }
+
+    // 	      return res.send('Authenticated! Data from arduino: ' + data);
+
+    // 	  })(req, res, next);
+    // 	});
 
 
     router.get('/:username/:password', function (req, res) {
@@ -34,8 +45,6 @@ module.exports =  function(router, passport){
         res.send(response);
 
     });
-
-
 
 
 
